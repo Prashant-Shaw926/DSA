6a65f005cbf9edc4e7a5a82acfbafac8280b90e6
Prashant Kumar Shaw <140432843+Prashant-Shaw926@users.noreply.github.com>
2026-09-02 20:31:02 +0530

    LC-backspace-string-compare [Runtime: N/A; Memory: N/A]

--- simulation/backspace-string-compare.cpp
+++ simulation/backspace-string-compare.cpp
@@ -3,67 +3,49 @@ public:
     bool backspaceCompare(string s, string t) {
 
         int n = s.size(), m = t.size();
-        int i = n - 1, j = m - 1, removeS = 0, removeT = 0;
+        int i = n - 1, j = m - 1;
 
-        while (i >= 0 && j >= 0) {
-            while (s[i] == '#') {
-                removeS++;
-                i--;
-            }
-            while (t[j] == '#') {
-                removeT++;
-                j--;
-            }
-            if (removeS != 0) {
-                while (removeS > 0 && i >= 0) {
-                    if (i == 0 && removeS > 0) {
-                        i--;
-                        break;
-                    }
-                    if (s[i] == '#') {
-                        removeS++;
-                    } else {
-                        removeS--;
-                    }
+        while (i >= 0 || j >= 0) {
 
+            int skipS = 0;
+
+            while (i >= 0) {
+                if (s[i] == '#') {
+                    skipS++;
                     i--;
-                    if (i >= 0 && s[i] == '#') {
-                        removeS++;
-                        i--;
-                    }
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break;
                 }
             }
-            if (removeT != 0) {
-                while (removeT > 0 && j >= 0) {
-                    if (j == 0 && removeT > 0) {
-                        j--;
-                        break;
-                    }
-                    if (t[j] == '#') {
-                        removeT++;
-                    } else {
-                        removeT--;
-                    }
 
+            int skipT = 0;
+
+            while (j >= 0) {
+                if (t[j] == '#') {
+                    skipT++;
                     j--;
-                    if (j >= 0 && t[j] == '#') {
-                        removeT++;
-                        j--;
-                    }
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
                 }
             }
-            if (i == -1 && j == -1) {
-                return true;
-            } else if ((i == -1 && j >= 0) || (j == -1 && i >= 0)) {
+
+            if (i >= 0 && j >= 0 && s[i] != t[j])
                 return false;
-            }
-            if (s[i] != t[j] && (s[i] != '#' && t[j] != '#')) {
+
+            if ((i >= 0) != (j >= 0))
                 return false;
-            } else {
-                i--;
-                j--;
-            }
+
+            i--;
+            j--;
         }
+
+        return true;
 
         return true;
     }
