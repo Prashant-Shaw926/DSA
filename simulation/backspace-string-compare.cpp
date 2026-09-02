b3ea3865540c861f627b16b1f38fdd2cc0624230
Prashant Kumar Shaw <140432843+Prashant-Shaw926@users.noreply.github.com>
2026-09-02 19:55:37 +0530

    LC-backspace-string-compare [Runtime: N/A; Memory: N/A]

--- simulation/backspace-string-compare.cpp
+++ simulation/backspace-string-compare.cpp
@@ -15,24 +15,47 @@ public:
                 j--;
             }
             if (removeS != 0) {
-                while (removeS > 0 && i > 0) {
+                while (removeS > 0 && i >= 0) {
+                    if (i == 0 && removeS > 0) {
+                        i--;
+                        break;
+                    }
                     if (s[i] == '#') {
                         removeS++;
                     } else {
                         removeS--;
                     }
+
                     i--;
+                    if (i >= 0 && s[i] == '#') {
+                        removeS++;
+                        i--;
+                    }
                 }
             }
             if (removeT != 0) {
-                while (removeT > 0 && j > 0) {
+                while (removeT > 0 && j >= 0) {
+                    if (j == 0 && removeT > 0) {
+                        j--;
+                        break;
+                    }
                     if (t[j] == '#') {
                         removeT++;
                     } else {
                         removeT--;
                     }
+
                     j--;
+                    if (j >= 0 && t[j] == '#') {
+                        removeT++;
+                        j--;
+                    }
                 }
+            }
+            if (i == -1 && j == -1) {
+                return true;
+            } else if ((i == -1 && j >= 0) || (j == -1 && i >= 0)) {
+                return false;
             }
             if (s[i] != t[j] && (s[i] != '#' && t[j] != '#')) {
                 return false;
