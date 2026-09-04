063e38b713e5078d108cc128211f702b3a92f0ce
Prashant Kumar Shaw <140432843+Prashant-Shaw926@users.noreply.github.com>
2026-09-04 23:29:12 +0530

    LC-shortest-unsorted-continuous-subarray [Runtime: N/A; Memory: N/A]

--- stack/shortest-unsorted-continuous-subarray.cpp
+++ stack/shortest-unsorted-continuous-subarray.cpp
@@ -6,14 +6,17 @@ public:
             return 0;
         }
         int firstIndex = -1, secondIndex = -1;
+        long long firstElement;
         int i = 0, j = 1;
 
         for (i = 0; i < n; i++) {
             for (j = i + 1; j < n; j++) {
                 if (nums[i] > nums[j] && firstIndex == -1) {
                     firstIndex = i;
+                    firstElement= nums[i];
                     secondIndex = j;
-                } else if (nums[i] > nums[j] && firstIndex != -1) {
+                } else if ((nums[i] > nums[j] && firstIndex != -1) ||
+                           firstElement > nums[j]) {
                     secondIndex = j;
                 }
             }
