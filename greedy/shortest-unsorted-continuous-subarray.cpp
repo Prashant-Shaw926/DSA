5e0b2ae5537be743ea546bab71cd3ca52e7d11f0
Prashant Kumar Shaw <140432843+Prashant-Shaw926@users.noreply.github.com>
2026-09-05 00:45:43 +0530

    LC-shortest-unsorted-continuous-subarray [Runtime: N/A; Memory: N/A]

--- greedy/shortest-unsorted-continuous-subarray.cpp
+++ greedy/shortest-unsorted-continuous-subarray.cpp
@@ -5,27 +5,50 @@ public:
         if (n == 1) {
             return 0;
         }
-        int firstIndex = -1, secondIndex = -1;
-        long long firstElement;
-        int i = 0, j = 1;
+        int start = -1, end = -1;
+        int i = 0;
+        long long max = INT_MIN, min = INT_MAX;
 
-        for (i = 0; i < n; i++) {
-            for (j = i + 1; j < n; j++) {
-                if (nums[i] > nums[j] && firstIndex == -1) {
-                    firstIndex = i;
-                    firstElement= nums[i];
-                    secondIndex = j;
-                } else if ((nums[i] > nums[j] && firstIndex != -1) ||
-                           firstElement > nums[j]) {
-                    secondIndex = j;
-                }
+        for (i = 0; i < n - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                start = i;
+                break;
             }
         }
 
-        if (firstIndex == -1) {
+        for (i = n - 1; i >= 1; i--) {
+            if (nums[i] < nums[i - 1]) {
+                end = i;
+                break;
+            }
+        }
+
+        if (start == -1) {
             return 0;
         }
 
-        return (secondIndex - firstIndex) + 1;
+        for (i = start; i <= end; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            } if (nums[i] < min) {
+                min = nums[i];
+            }
+        }
+
+        for (i = 0; i < n; i++) {
+            if (nums[i] > min) {
+                start = i;
+                break;
+            }
+        }
+
+        for (i = n - 1; i >= 0; i--) {
+            if (nums[i] < max) {
+                end = i;
+                break;
+            }
+        }
+
+        return (end - start) + 1;
     }
 };
